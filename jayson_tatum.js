67840e99542e7538249337677303584fa54c172d
Trevor Reynolds <trevor@rfpio.com>
2022-06-13 17:01:00 -0500

    another minor change

--- jayson_tatum.js
+++ jayson_tatum.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 const skills = ['defense','scoring','play-making','great waves']
+=======
+const skills = ['defense','scoring','playmaking','great waves']
+>>>>>>> another minor change
 const team = 'Boston Celtics'
 
 const go3for17 = () => {
